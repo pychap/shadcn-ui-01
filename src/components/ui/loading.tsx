827996cf8f63f892a1835fbb4552ad1644ae0ae4
Peter Chapman <pchapman@ptc.com>
2024-02-01 14:01:39 -0800

    Shadcn UI initial files

---
+++ src/components/ui/loading.tsx
@@ -0,0 +1,13 @@
+import SkeletonCard from "../SkeletonCard";
+
+export default function loading() {
+  return (
+    <main>
+      <div className="grid grid-cols-3 gap-8">
+        {"abcdefghi".split("").map((letter) => (
+          <SkeletonCard key={letter} />
+        ))}
+      </div>
+    </main>
+  );
+}
